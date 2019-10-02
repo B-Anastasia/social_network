b91f78bd0027b15b1246af6f23574f73fb38dfc9
Anastasia Boshniak <boshniak.a@gmail.com>
2019-10-02 18:35:41 +0300

    store data

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import App from "./App";
-import {addPost, updateMessagePost} from "./redux/state";
+// import {addPost, updateMessagePost} from "./redux/state";
 
 
  let rerenderEntireTree= () => {
-    ReactDOM.render( <App state={state} addPost={addPost} updateMessagePost={updateMessagePost} />, document.getElementById('root'));
+    ReactDOM.render( <App state={store} />, document.getElementById('root'));
 }
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+store.rerenderEntireTree(store.getState);
+store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,75 +1,155 @@
 
-let rerenderEntireTree= () =>{
-    console.log("State was changed");
-}
+let store = {
+    _state: {
 
-
-let state = {
-
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, this is my first post!', likes: "15"},
-            {id: 2, message: 'I like React', likes: "20"}
-        ],
-        newPostMessage: 'New post'
-    },
-    dialogsPage: {
-        dialogs: [
-            {id:1, name:'Andrey', img:"/img/users/user_1.jpg"},
-            {id:2, name:'Viktor', img:"/img/users/user_2.jpg"},
-            {id:3, name:'Katerina', img:"/img/users/user_3.jpg"},
-            {id:4, name:'Tanya', img:"/img/users/user_4.jpg"},
-            {id:5, name:'Anastasia', img:"/img/users/user_5.jpg"},
-            {id:6, name:'Alisa', img:"/img/users/user_6.jpg"},
-            {id:7, name:'Ludmila', img:"/img/users/user_7.jpg"},
-            {id:8, name:'Sergey', img:"/img/users/user_8.jpg"}
-        ],
-        messages:{
-            messagesFriendOne: [
-                {id:1, message:'Hi'},
-                {id:2, message:'How are you?'},
-                {id:3, message:'This is a good start!'},
-                {id:4, message:'Yo'},
-                {id:5, message:'I like you!'},
-                {id:6, message:'Buy!'}
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, this is my first post!', likes: "15"},
+                {id: 2, message: 'I like React', likes: "20"}
             ],
-            messagesFriendTwo: [
-                {id:1, message:'Yo!'},
-                {id:2, message:'Fine, how are you?'},
-                {id:3, message:'Great!'},
-                {id:4, message:'Yo'},
-                {id:5, message:'I like you too!'},
-                {id:6, message:'Buy!'}
+            newPostMessage: 'New post'
+        },
+        dialogsPage: {
+            dialogs: [
+                {id:1, name:'Andrey', img:"/img/users/user_1.jpg"},
+                {id:2, name:'Viktor', img:"/img/users/user_2.jpg"},
+                {id:3, name:'Katerina', img:"/img/users/user_3.jpg"},
+                {id:4, name:'Tanya', img:"/img/users/user_4.jpg"},
+                {id:5, name:'Anastasia', img:"/img/users/user_5.jpg"},
+                {id:6, name:'Alisa', img:"/img/users/user_6.jpg"},
+                {id:7, name:'Ludmila', img:"/img/users/user_7.jpg"},
+                {id:8, name:'Sergey', img:"/img/users/user_8.jpg"}
+            ],
+            messages:{
+                messagesFriendOne: [
+                    {id:1, message:'Hi'},
+                    {id:2, message:'How are you?'},
+                    {id:3, message:'This is a good start!'},
+                    {id:4, message:'Yo'},
+                    {id:5, message:'I like you!'},
+                    {id:6, message:'Buy!'}
+                ],
+                messagesFriendTwo: [
+                    {id:1, message:'Yo!'},
+                    {id:2, message:'Fine, how are you?'},
+                    {id:3, message:'Great!'},
+                    {id:4, message:'Yo'},
+                    {id:5, message:'I like you too!'},
+                    {id:6, message:'Buy!'}
+                ]
+            }
+        },
+        navbar: [
+            {id:2, path: "/dialogs", name: "Messages" },
+            {id:3, path: "/news", name: "News" },
+            {id:4, path: "/music", name: "Music" },
+            {id:5, path: "/settings", name: "Settings" },
+            {id:5, path: "/friends", name: "Friends" },
         ]
-        }
     },
-    navbar: [
-        {id:2, path: "/dialogs", name: "Messages" },
-        {id:3, path: "/news", name: "News" },
-        {id:4, path: "/music", name: "Music" },
-        {id:5, path: "/settings", name: "Settings" },
-        {id:5, path: "/friends", name: "Friends" },
-    ]
-}
-window.state=state;
+    rerenderEntireTree(){
+        console.log("State was changed");
+    },
+    setState(){
 
-export const addPost = () => {
-    let newPost= {
-        id: 3,
-        message: state.profilePage.newPostMessage,
-        likes: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostMessage='';
-    rerenderEntireTree(state);
-}
-export const updateMessagePost = (newText) => {
-    state.profilePage.newPostMessage = newText;
-    rerenderEntireTree(state);
+    },
+    getState(){
+        return this._state;
+    },
+    addPost(){
+        let state= this._state;
+        let newPost= {
+            id: 3,
+            message: state.profilePage.newPostMessage,
+            likes: 0
+        };
+        state.profilePage.posts.push(newPost);
+        state.profilePage.newPostMessage='';
+        this.rerenderEntireTree(state);
+    },
+    updateMessagePost(newText){
+        let state= this._state;
+        state.profilePage.newPostMessage = newText;
+        this.rerenderEntireTree(state);
+    },
+    subscribe(observer){
+        this.rerenderEntireTree=observer;
+    }
 }
 
-export const subscribe = (observer) => {
-rerenderEntireTree=observer;  //pattern - observer
-}
+export default store;
 
-export default state;
+// let rerenderEntireTree= () =>{
+//     console.log("State was changed");
+// }
+
+// let state = {
+//
+//     profilePage: {
+//         posts: [
+//             {id: 1, message: 'Hi, this is my first post!', likes: "15"},
+//             {id: 2, message: 'I like React', likes: "20"}
+//         ],
+//         newPostMessage: 'New post'
+//     },
+//     dialogsPage: {
+//         dialogs: [
+//             {id:1, name:'Andrey', img:"/img/users/user_1.jpg"},
+//             {id:2, name:'Viktor', img:"/img/users/user_2.jpg"},
+//             {id:3, name:'Katerina', img:"/img/users/user_3.jpg"},
+//             {id:4, name:'Tanya', img:"/img/users/user_4.jpg"},
+//             {id:5, name:'Anastasia', img:"/img/users/user_5.jpg"},
+//             {id:6, name:'Alisa', img:"/img/users/user_6.jpg"},
+//             {id:7, name:'Ludmila', img:"/img/users/user_7.jpg"},
+//             {id:8, name:'Sergey', img:"/img/users/user_8.jpg"}
+//         ],
+//         messages:{
+//             messagesFriendOne: [
+//                 {id:1, message:'Hi'},
+//                 {id:2, message:'How are you?'},
+//                 {id:3, message:'This is a good start!'},
+//                 {id:4, message:'Yo'},
+//                 {id:5, message:'I like you!'},
+//                 {id:6, message:'Buy!'}
+//             ],
+//             messagesFriendTwo: [
+//                 {id:1, message:'Yo!'},
+//                 {id:2, message:'Fine, how are you?'},
+//                 {id:3, message:'Great!'},
+//                 {id:4, message:'Yo'},
+//                 {id:5, message:'I like you too!'},
+//                 {id:6, message:'Buy!'}
+//         ]
+//         }
+//     },
+//     navbar: [
+//         {id:2, path: "/dialogs", name: "Messages" },
+//         {id:3, path: "/news", name: "News" },
+//         {id:4, path: "/music", name: "Music" },
+//         {id:5, path: "/settings", name: "Settings" },
+//         {id:5, path: "/friends", name: "Friends" },
+//     ]
+// }
+// window.state=state;
+
+// export const addPost = () => {
+//     let newPost= {
+//         id: 3,
+//         message: state.profilePage.newPostMessage,
+//         likes: 0
+//     };
+//     state.profilePage.posts.push(newPost);
+//     state.profilePage.newPostMessage='';
+//     rerenderEntireTree(state);
+// }
+
+// export const updateMessagePost = (newText) => {
+//     state.profilePage.newPostMessage = newText;
+//     rerenderEntireTree(state);
+// }
+
+// export const subscribe = (observer) => {
+// rerenderEntireTree=observer;  //pattern - observer
+// }
+
+// export default state;
