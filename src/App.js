d5a44154fe5f3d3a406f2269a10bcb9335cd50c7
Anastasia Boshniak <boshniak.a@gmail.com>
2019-10-11 12:57:50 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -7,27 +7,26 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Setting/Settings";
-import { BrowserRouter, Route } from "react-router-dom";
+import {Route} from "react-router-dom";
 import Friends from "./components/Friends/Friends";
 
 const App = (props) => {
-  return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar state={props.state.navbar} user={props.state.dialogsPage} />
-        <div className="app-wrapper-content">
-          <Route exact path="/" render={()=><Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />} />
-          <Route path="/dialogs" render={()=><Dialogs state={props.state.dialogsPage} dispatch={props.dispatch} />} />
-          <Route path="/news" component={News} />
-          <Route path="/music" component={Music} />
-          <Route path="/settings" component={Settings} />
-          <Route path="/friends" render = { () => <Friends state={props.state.dialogsPage.dialogs} />} />
-
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar state={props.state.navbar} user={props.state.dialogsPage}/>
+            <div className="app-wrapper-content">
+                <Route exact path="/"
+                       render={() => <Profile profilePage={props.state.profilePage} dispatch={props.dispatch}/>}/>
+                <Route path="/dialogs"
+                       render={() => <Dialogs state={props.state.dialogsPage} dispatch={props.dispatch}/>}/>
+                <Route path="/news" component={News}/>
+                <Route path="/music" component={Music}/>
+                <Route path="/settings" component={Settings}/>
+                <Route path="/friends" render={() => <Friends state={props.state.dialogsPage.dialogs}/>}/>
+            </div>
         </div>
-      </div>
-    </BrowserRouter>
-  );
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/state";
+import store from "./redux/redux-store";
+
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 // import {addPost, updateMessagePost} from "./redux/state";
 
 
  let rerenderEntireTree= (state) => {
-    ReactDOM.render( <App state={state} dispatch={store.dispatch.bind(store)}/>, document.getElementById('root'));
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} dispatch={store.dispatch.bind(store)}/>
+        </BrowserRouter>, document.getElementById('root'));
 }
 rerenderEntireTree(store.getState());
 store.subscribe(rerenderEntireTree);
